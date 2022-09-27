d7df902d6e244ae10063669d5f0c3a2ffff7f69b
clayicarus <1072464850@qq.com>
2022-09-28 00:17:31 +0800

    echo server with Reactor

---
+++ echo_serv/lib/lnp.h
@@ -0,0 +1,164 @@
+/* include unph */
+/* Our own header.  Tabs are set for 4 spaces, not 8 */
+
+#ifndef	__lnp_h
+#define	__lnp_h
+
+/* If anything changes in the following list of #includes, must change
+   acsite.m4 also, for configure's tests. */
+
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<sys/time.h>	/* timeval{} for select() */
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	/* for S_xxx file mode constants */
+#include	<sys/uio.h>		/* for iovec{} and readv/writev */
+#include	<unistd.h>
+#include	<sys/wait.h>
+#include	<sys/un.h>		/* for Unix domain sockets */
+
+# include	<sys/select.h>	/* for convenience */
+
+# include	<poll.h>		/* for convenience */
+
+# include	<strings.h>		/* for convenience */
+
+/* Three headers are normally needed for socket/file ioctl's:
+ * <sys/ioctl.h>, <sys/filio.h>, and <sys/sockio.h>.
+ */
+# include	<sys/ioctl.h>
+
+# include	<pthread.h>
+
+/* Define bzero() as a macro if it's not in standard C library. */
+#define	bzero(ptr,n)		memset(ptr, 0, n)
+/* $$.If bzero$$ */
+/* $$.If memset$$ */
+
+/* Following could be derived from SOMAXCONN in <sys/socket.h>, but many
+   kernels still #define it as 5, while actually supporting many more */
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+
+/* Miscellaneous constants */
+#define	MAXLINE		4096	/* max text line length */
+#define	BUFFSIZE	8192	/* buffer size for reads and writes */
+
+/* Define some port number that can be used for our examples */
+#define	SERV_PORT		 1919			/* TCP and UDP */
+#define	SERV_PORT_STR	"9877"			/* TCP and UDP */
+
+/* Following shortens all the typecasts of pointer arguments: */
+#define	SA	struct sockaddr
+
+#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+					/* default file access permissions for new files */
+#define	DIR_MODE	(FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+					/* default permissions for new directories */
+
+typedef	void	Sigfunc(int);	/* for signal handlers */
+
+			/* prototypes for our own library wrapper functions */
+void	 Connect_timeo(int, const SA *, socklen_t, int);
+int		 Family_to_level(int);
+struct addrinfo *Host_serv(const char *, const char *, int, int);
+const char		*Inet_ntop(int, const void *, char *, size_t);
+void			 Inet_pton(int, const char *, void *);
+char			*If_indextoname(unsigned int, char *);
+unsigned int		 If_nametoindex(const char *);
+struct if_nameindex	*If_nameindex(void);
+char   **My_addrs(int *);
+ssize_t	 Read_fd(int, void *, size_t, int *);
+int		 Readable_timeo(int, int);
+
+Sigfunc *Signal(int, Sigfunc *);
+Sigfunc *Signal_intr(int, Sigfunc *);
+int		 Sock_bind_wild(int, int);
+char	*Sock_ntop(const SA *, socklen_t);
+char	*Sock_ntop_host(const SA *, socklen_t);
+int		 Sockfd_to_family(int);
+int		 Tcp_connect(const char *, const char *);
+int		 Tcp_listen(const char *, const char *, socklen_t *);
+int		 Udp_client(const char *, const char *, SA **, socklen_t *);
+int		 Udp_connect(const char *, const char *);
+int		 Udp_server(const char *, const char *, socklen_t *);
+ssize_t	 Write_fd(int, void *, size_t, int);
+int		 Writable_timeo(int, int);
+
+			/* prototypes for our Unix wrapper functions: see {Sec errors} */
+void	*Calloc(size_t, size_t);
+void	 Close(int);
+void	 Dup2(int, int);
+int		 Fcntl(int, int, int);
+void	 Gettimeofday(struct timeval *, void *);
+int		 Ioctl(int, int, void *);
+pid_t	 Fork(void);
+void	*Malloc(size_t);
+int	 Mkstemp(char *);
+void	*Mmap(void *, size_t, int, int, int, off_t);
+int		 Open(const char *, int, mode_t);
+void	 Pipe(int *fds);
+ssize_t	 Read(int, void *, size_t);
+void	 Sigaddset(sigset_t *, int);
+void	 Sigdelset(sigset_t *, int);
+void	 Sigemptyset(sigset_t *);
+void	 Sigfillset(sigset_t *);
+int		 Sigismember(const sigset_t *, int);
+void	 Sigpending(sigset_t *);
+void	 Sigprocmask(int, const sigset_t *, sigset_t *);
+char	*Strdup(const char *);
+long	 Sysconf(int);
+void	 Sysctl(int *, u_int, void *, size_t *, void *, size_t);
+void	 Unlink(const char *);
+pid_t	 Wait(int *);
+pid_t	 Waitpid(pid_t, int *, int);
+void	 Write(int, void *, size_t);
+
+			/* prototypes for our stdio wrapper functions: see {Sec errors} */
+void	 Fclose(FILE *);
+FILE	*Fdopen(int, const char *);
+char	*Fgets(char *, int, FILE *);
+FILE	*Fopen(const char *, const char *);
+void	 Fputs(const char *, FILE *);
+
+			/* prototypes for our socket wrapper functions: see {Sec errors} */
+int		 Accept(int, SA *, socklen_t *);
+void	 Bind(int, const SA *, socklen_t);
+void	 Connect(int, const SA *, socklen_t);
+void	 Getpeername(int, SA *, socklen_t *);
+void	 Getsockname(int, SA *, socklen_t *);
+void	 Getsockopt(int, int, int, void *, socklen_t *);
+
+void	 Listen(int, int);
+int		 Poll(struct pollfd *, unsigned long, int);
+ssize_t	 Readline(int, void *, size_t);
+ssize_t	 Readn(int, void *, size_t);
+ssize_t	 Recv(int, void *, size_t, int);
+ssize_t	 Recvfrom(int, void *, size_t, int, SA *, socklen_t *);
+ssize_t	 Recvmsg(int, struct msghdr *, int);
+int		 Select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
+void	 Send(int, const void *, size_t, int);
+void	 Sendto(int, const void *, size_t, int, const SA *, socklen_t);
+void	 Sendmsg(int, const struct msghdr *, int);
+void	 Setsockopt(int, int, int, const void *, socklen_t);
+void	 Shutdown(int, int);
+int		 Sockatmark(int);
+int		 Socket(int, int, int);
+void	 Socketpair(int, int, int, int *);
+void	 Writen(int, void *, size_t);
+
+void	 err_dump(const char *, ...);
+void	 err_msg(const char *, ...);
+void	 err_quit(const char *, ...);
+void	 err_ret(const char *, ...);
+void	 err_sys(const char *, ...);
+
+#endif    /* __lnp_h */
---
+++ echo_serv/main.cpp
@@ -0,0 +1,117 @@
+//
+// Created by clay on 22-9-27.
+//
+
+#include "lib/lnp.h"
+
+#include<iostream>
+#include<array>
+#include <algorithm>
+
+using namespace std;
+
+void onMessage(int sockfd, array<char, MAXLINE> &buf);
+
+int main(int argc, char *argv[])
+{
+    int maxi, maxfd, listenfd;
+    array<int, FD_SETSIZE> clients; // client is sockfd.
+    array<char, MAXLINE> buf;
+    fd_set allset;  // use of record all need to be listened.
+    socklen_t clilen;
+    sockaddr_in cliaddr, servaddr;
+
+    listenfd = Socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    Bind(listenfd, reinterpret_cast<sockaddr *> (&servaddr), sizeof(servaddr));
+
+    Listen(listenfd, LISTENQ);
+
+    maxfd = listenfd;
+    maxi = -1;
+    clients.fill(-1);
+
+    FD_ZERO(&allset);
+    FD_SET(listenfd, &allset);
+
+    for( ; ; ) {
+        fd_set rset;
+        int nready;
+        int connfd, sockfd; // conn is only new connected fd.
+        int i;
+
+        rset = allset;
+
+        cout << "blocking on select() " << endl;
+        nready = Select(maxfd + 1, &rset, NULL, NULL, NULL);
+        cout << nready << " fd(s) readable " << endl;
+
+        if(FD_ISSET(listenfd, &rset)) {
+            /* nready >= 1 ? */
+            clilen = sizeof(cliaddr);
+            connfd = Accept(listenfd, reinterpret_cast<sockaddr *>(&cliaddr), &clilen); // new connection
+            /*onConnection*/
+            cout << "new client connected" << endl;
+            for(i = 0; i < clients.size(); ++i) {
+                if(clients[i] < 0){
+                    clients[i] = connfd;
+                    break;
+                }
+            }
+            if(i == clients.size()) {
+                err_quit("too many clients");
+            }
+
+            FD_SET(connfd, &allset);    // add new client to set.
+            maxfd = max(maxfd, connfd);
+            maxi = max(maxi, i);        // to check probable available clients.
+
+            if(--nready <= 0) {
+                cout << "no clients readable" << endl;
+                continue;               // only listen sock readable.
+            } else {
+                cout << nready << "client(s) readable" << endl;
+            }
+        }
+
+        for(i = 0; i <= maxi; ++i) {    // pay attention to i <= maxi
+            if((sockfd = clients[i]) < 0) {
+                continue;
+            }
+            if(FD_ISSET(sockfd, &rset)) {
+                /* OnMessage callback */
+                ssize_t n;
+                cout << "client " << sockfd << " readable" << endl;
+                cout << "blocking on read()" << endl;
+                if((n = Read(sockfd, buf.data(), buf.size())) == 0) {
+                    /* connection closed by client */
+                    Close(sockfd);  // send FIN; go to LAST_ACK.
+                    FD_CLR(sockfd, &allset);
+                    clients[i] = -1;
+                    // change maxi.
+                    cout << "client " << sockfd << " disconnected" << endl;
+                } else {
+                    cout << "read " << n << " bytes" << endl;
+                    Writen(sockfd, buf.data(), n);  // directly write what you read.
+                }
+
+                if(--nready <= 0) {
+                    // no clients readable
+                    break;
+                }
+            }
+        }
+    }
+
+    // return 0;
+}
+
+void onMessage(int sockfd, array<char, MAXLINE> &buf)
+{
+
+}
